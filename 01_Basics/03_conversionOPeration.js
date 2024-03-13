cfedbb5569affb92b6e1cf93903e029a3e829816
Anupam Kumari Mishra <amishra20feb@gmail.com>
2024-03-13 23:22:18 +0530

    conversion of data type

---
+++ 01_Basics/03_conversionOPeration.js
@@ -0,0 +1,21 @@
+let score="anupam"
+console.log(typeof score); //first method to show types of data
+console.log(typeof (score)); //second first method to show types of data
+let valueInNumber =Number(score);
+console.log(typeof valueInNumber );
+console.log(valueInNumber );
+//77 =>77
+//77sdef =>NaN(not a number)
+//true =>1 ,false=>0
+//"anupam"=>NaN
+let IsLoggedIn ="anupam";
+let booleanLoggedIn= Boolean(IsLoggedIn);
+console.log(typeof booleanLoggedI);
+console.log(booleanLoggedIn);
+//1 =>true ,0=>false
+//"" =>false
+//anupam =>true
+let someNumber=55
+let stringNumber=String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
