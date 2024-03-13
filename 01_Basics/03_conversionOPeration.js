29384a9540c5f0c5b51f2e5d4230016716accc4b
Anupam Kumari Mishra <amishra20feb@gmail.com>
2024-03-14 00:06:20 +0530

    operations

--- 01_Basics/03_conversionOPeration.js
+++ 01_Basics/03_conversionOPeration.js
@@ -19,3 +19,28 @@ let someNumber=55
 let stringNumber=String(someNumber);
 console.log(typeof stringNumber);
 console.log(stringNumber);
+/****************************************Operators**************************************/
+let value =5
+let negValue =-value;
+console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+let str1 ="hello"
+let str2 =" anupam"
+let str3= str1+str2
+console.log(str3);
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+4+"5");
+console.log(+true);
+//console.log(true+);
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
