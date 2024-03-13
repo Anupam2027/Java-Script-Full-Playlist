d10e62a009e98e93acba5105fa40bc6e497f90aa
Anupam Kumari Mishra <amishra20feb@gmail.com>
2024-03-13 21:10:45 +0530

    fisrt commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=120394
+let accountEmails="anupam2gmail.com"
+var accountPassword="123456"
+let accountState;
+//accountCity="kolkata";
+
+accountEmails="adkrgrk"
+accountPassword="348359"
+console.log(accountId);
+console.table([accountId,accountEmails,accountPassword,accountState])
+/*
+prefer to not use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) // we are using node js not browser
+console.log(3
+    +3); // code readibility should be high
+    console.log("anupam");
+
+    //data types
+    let name="anupam"
+    let age=18
+    let IsloggedIn = false;
+
+    //numeric => 2 to power 53
+    //bigint
+    //string =>""
+    //boolean => true,false
+    //null =>standalone value
+    //undefined =>
+    //symbol =>uniqueness
+
+    //object
+    console.log(typeof age);
+    console.log(typeof null); //null is a object
+    console.log(typeof undefined); //undefined not null
