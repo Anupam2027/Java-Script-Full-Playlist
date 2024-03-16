27dc90f2cc2c4d0748e28fbba88f7da3f5c31a50
Anupam Kumari Mishra <amishra20feb@gmail.com>
2024-03-16 21:07:47 +0530

    comparision between data types and data

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,22 @@
+//normal comparision between same data types
+console.log(2>5);
+console.log(2>=5);
+console.log(2<=5);
+console.log(2<5);
+console.log(2==5);
+console.log(4!=5);
+// comparision between different  data types
+console.log("2">5);
+console.log("25">5);
+//its convert into different datatypes automatically ,but some time we will fail in prediction
+//compare null value with '0'
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+//compare undefined value with '0'
+console.log(undefined >0);
+console.log(undefined == 0);
+console.log(undefined <= 0);
+//'==' , '==='
+console.log("2"==2); //normally check comparison ,and automatically check
+console.log("2"===2); //it is strictly check data types with data 
